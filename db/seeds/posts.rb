75f49761b7950c4899ffc7c8fb612111f72e8477
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-11-11 23:23:00 +0100

    changed seeds

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Post.create(title: 'First Post', body: 'This is the body of the first post.' * 10, category: Category.first, user: User.first)
+Post.create(title: 'First Post', body: 'This is the body of the first post.' * 10, category: Category.first, creator: User.first)
