5ebca1c3c38dcc8967599e1002a0ca0f271602cd
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-12-27 12:35:53 +0100

    fix 16 v2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    post.likes.find_or_create_by(user: current_user)
+    @post.likes.find_or_create_by(user: current_user)
     redirect_to @post
   end
 
