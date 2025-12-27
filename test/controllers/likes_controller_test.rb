58fa493981d6288b8b0508e0c067d9334d0e2bb8
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-12-27 12:30:57 +0100

    fixed likes controller tests

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -13,14 +13,18 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
     assert_difference('PostLike.count', 1) do
       post post_likes_url(@post)
     end
-    assert_redirected_to post_path(@post)
+
+    assert_no_difference('PostLike.count') do
+      post post_likes_url(@post)
+    end
+    assert_redirected_to @post
   end
 
   test 'should destroy like' do
-    @like = PostLike.create!(post: @post, user: @user)
+    @like = PostLike.find_or_create_by!(post: @post, user: @user)
     assert_difference('PostLike.count', -1) do
       delete post_like_url(@post, @like)
     end
-    assert_redirected_to post_path(@post)
+    assert_redirected_to @post
   end
 end
