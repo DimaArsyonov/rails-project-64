acfc03315a2f491e24f2e33da8503ca77e1c66c3
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2025-12-27 11:50:52 +0100

    fix 13 v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, inverse_of: :creator, dependent: :destroy
-
+  has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 end
